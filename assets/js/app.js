01516bc77a33d4243a483e879470da9eda5fda52
CameronFarquhar <cameronfarquhar@gmail.com>
2021-04-13 16:06:28 -0400

    building frame for chart

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,36 @@
 // @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+var svg = d3.select(".chart")
+.append("svg")
+.attr("width",svgWidth)
+.attr("height", svgHeight);
+
+console.log(svg);
+
+var charGroup = svg.append("g")
+.attr("transform", `translate(${margin.left}, ${margin.right})`);
+
+var chosenXAxis = "poverty";
+
+function xScale(data, d){
+    var xLinearScale = d3.scaleLinear() 
+    .domain([d3.min(data, d => d[chosenXAxis]) * 0.8,
+        d3.max(data, d =>d[chosenXAxis]) * 1.2
+        ])
+        .range([0, width]);
+        
+    return xLinearScale;
+    }
+
